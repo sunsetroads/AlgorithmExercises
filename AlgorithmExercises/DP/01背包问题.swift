782cd3cb4f7446193c2f35d7858b8f721064ddf3
sunsetroads <ningvvy@gmail.com>
2020-04-16 19:02:05 +0800

    add db problem

---
+++ AlgorithmExercises/DP/01背包问题.swift
@@ -0,0 +1,36 @@
+fileprivate let problem =
+"""
+给你一个可装载重量为 W 的背包和 N 个物品，每个物品有重量和价值两个属性。其中第 i 个物品的重量为 wt[i]，价值为 val[i]，现在让你用这个背包装物品，最多能装的价值是多少？
+
+举个简单的例子，输入如下：
+N = 3, W = 4
+wt = [2, 1, 3]
+val = [4, 2, 3]
+算法返回 6，选择前两件物品装进背包，总重量 3 小于 W，可以获得最大价值 6。
+"""
+
+fileprivate let remind =
+"""
+dp 问题通常就是穷举所有可能，找出最佳方案，只是消除了一些重叠子问题
+dp[i][j] 表示 第 i 个物品 j容量下的最大价值
+
+dp[i - 1][j] 不装第 i 个物品
+val[i - 1] + dp[i-1][w - wt[i - 1]] 装第 i 个物品
+"""
+
+func maxValue(_ w:Int, _ n:Int, _ wt:[Int], _ val:[Int]) -> Int {
+    
+    var dp = Array(repeating: Array(repeating: 0, count: w + 1), count: n + 1)
+    
+    for i in 1...n {
+        for j in 1...w {
+            if wt[i - 1] > j {
+                dp[i][j] = dp[i - 1][j]
+            } else {
+                dp[i][j] = max(dp[i - 1][j], val[i - 1] + dp[i-1][w - wt[i - 1]])
+            }
+        }
+    }
+    
+    return dp[n][w];
+}
---
+++ AlgorithmExercises/DP/最长回文子序列.swift
@@ -0,0 +1,82 @@
+/*
+* @lc app=leetcode.cn id=516 lang=swift
+*
+* [516] 最长回文子序列
+*
+* https://leetcode-cn.com/problems/longest-palindromic-subsequence/description/
+*
+* algorithms
+* Medium (48.11%)
+* Likes:    178
+* Dislikes: 0
+* Total Accepted:    16.6K
+* Total Submissions: 31.3K
+* Testcase Example:  '"bbbab"'
+*
+* 给定一个字符串s，找到其中最长的回文子序列。可以假设s的最大长度为1000。
+*
+* 示例 1:
+* 输入:
+*
+*
+* "bbbab"
+*
+*
+* 输出:
+*
+*
+* 4
+*
+*
+* 一个可能的最长回文子序列为 "bbbb"。
+*
+* 示例 2:
+* 输入:
+*
+*
+* "cbbd"
+*
+*
+* 输出:
+*
+*
+* 2
+*
+*
+* 一个可能的最长回文子序列为 "bb"。
+*
+*/
+
+fileprivate let remind =
+"""
+dp[i][j] 表示 s[i...j] 的最长回文子串
+
+s[i] == s[j] 时 dp[i][j] = dp[i + 1][j - 1] + 2
+s[i] != s[j] 时取 dp[i + 1][j] dp[i][j - 1]的最大值
+
+麻烦的地方在于如何确定 base case
+j 必需大于 i
+j == i 时返回 1
+
+dp[i][j] 依赖于dp[i + 1][j] dp[i][j - 1] dp[i + 1][j - 1] 画下 DP table 可以看出来需要反着遍历 i，才能先求出 dp[i][j] 依赖的状态。
+而对于 j，也只需要遍历 大于 i 的部分
+"""
+
+func longestPalindromeSubseq(_ s: String) -> Int {
+    let sArr = Array(s)
+    var dp = Array(repeating: Array(repeating: 0, count: s.count), count: s.count)
+    for i in 0..<s.count {
+        dp[i][i] = 1
+    }
+    
+    for i in (0..<sArr.count - 1).reversed() {
+        for j in i + 1..<sArr.count{
+            if sArr[i] == sArr[j] {
+                dp[i][j] = dp[i + 1][j - 1] + 2
+            } else {
+                dp[i][j] = max(dp[i + 1][j], dp[i][j - 1])
+            }
+        }
+    }
+    return dp[0][s.count - 1]
+}
